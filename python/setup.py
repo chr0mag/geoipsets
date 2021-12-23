e4fb4a4026f5450d2dcc42c1c54c19970d39ee14
Julian Phillips <phillips.julian@gmail.com>
2021-12-23 10:54:11 -0800

    Add Python 3.10 support

--- python/setup.py
+++ python/setup.py
@@ -28,6 +28,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
