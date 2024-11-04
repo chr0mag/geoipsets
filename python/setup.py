d7eba7a0bc199d4ad5152d703dc189e17f4ac569
Julian Phillips <phillips.julian@gmail.com>
2024-11-04 12:53:21 -0800

    Drop Python 3.8 support
    Python 3.8 is EOL, although it still works fine.

--- python/setup.py
+++ python/setup.py
@@ -26,7 +26,6 @@ setup(
         "Operating System :: POSIX :: Linux",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
